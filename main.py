95263d4e18638d70e4240eb276b69b420bac369c
Pranay <pranay.r22@gmail.com>
2023-09-22 20:01:53 -0700

    create initial file

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# TODO 1 - scrape data from Quiet Light Website (use headers w/ BeautifulSoup)
+
+# TODO 2 - create json file with all Quiet Light listings (must be saved to external file)
+
+# TODO 3 - scan each listing title in Quiet Light with the external file
+
+# TODO 4 - If listing title does not exist, create new entry in json file
+
+#TODO 5 - Send new listing information as text
